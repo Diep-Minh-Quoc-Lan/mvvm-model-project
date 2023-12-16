f0f955be2614f8b8c4cec960956839fd06164e45
quoclan0911 <quoclan0911@gmail.com>
2023-12-16 22:43:03 +0700

    Initial commit

---
+++ MVVM_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Project
+//
+//  Created by STDIO on 16/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
